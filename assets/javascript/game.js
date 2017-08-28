09d20ba4273b3df0661c6f411fc4b00c85ffa32e
Gene Williams <GeneWillb@Gmail.com>
2017-08-28 00:35:31 -0400

    page layout complete ready for jQuery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,4 +3,28 @@ $(document).ready( function() {
     var hp;
     var counterAttack;
     
+    //generate hp attack power and counter attack power for all four characters
+    
+    
+    
+    //select the different characters
+    
+    //select character have selected character move into attack zone
+    
+    //select enemy and have selected enemy move into the defenders zone
+    
+    //have character player attack (and increase attack power by attack power every time) have enemy counter attack automatically
+    
+    //after defender is defeated move back to selection area and gray out
+    
+    //select new enemy and move them to defender zone
+    
+    //if player character is defeated reset game
+    
+    
+    
+    
+    
+    
+    
 });
