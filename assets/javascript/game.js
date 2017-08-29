a04498d621abd67dce7ef830699ed4550a95c334
Gene Williams <GeneWillb@Gmail.com>
2017-08-28 21:23:49 -0400

    basic functionality, can click on character and choose yours, then the rest get put into the defender section

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,35 @@ $(document).ready( function() {
     var attack;
     var hp;
     var counterAttack;
+    var charChosen;
+    var charSelected = false;
+    var defChosen;
+    
+    
+    
+    //select your character and then move other characters into the enemy section
+    
+        $(".charContainer").on("click", function () {
+            if (charSelected === false) {
+                var yourChar = this.id;
+                charChosen = yourChar;
+                charSelected = true;
+                if (charChosen === yourChar) {
+                    $("#" + yourChar).removeClass("charContainer").addClass("selectedCharContainer").appendTo("#yourChar");
+                }
+                console.log(charSelected);
+                if (charSelected === true) {
+                    $(".charContainer").appendTo("#defenders-area");
+                }
+            }
+        });
+    
+       
+    
+    
+ 
     
     //generate hp attack power and counter attack power for all four characters
-    
-    
-    
-    //select the different characters
     
     //select character have selected character move into attack zone
     
@@ -20,11 +43,6 @@ $(document).ready( function() {
     //select new enemy and move them to defender zone
     
     //if player character is defeated reset game
-    
-    
-    
-    
-    
     
     
 });
