a9c010d933a21101af05af033e9b4f6709fc4b44
Gene Williams <GeneWillb@Gmail.com>
2017-08-29 18:19:00 -0400

    nested data structures

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,10 +10,22 @@ $(document).ready( function() {
     var charSelected = false;
     var defChosen;
     var defSelected = false;
-   
     
+    //assign attack hp and counterAttack to each character
+    var charObject = {
+        char1: [{attack: 18, hp: 150, counterAttack: 9}],
+        char2: [{attack: 8, hp: 100, counterAttack: 5}],
+        char3: [{attack: 22,  hp: 200,  counterAttack: 18}],
+        char4: [{attack: 12,  hp: 250, counterAttack: 15}]
+    };
     
-    //select your character and then move other characters into the enemy section
+    //displays the characters health
+    $("#char1Health").html(charObject.char1[0].hp);
+    $("#char2Health").html(charObject.char2[0].hp);
+    $("#char3Health").html(charObject.char3[0].hp);
+    $("#char4Health").html(charObject.char4[0].hp);
+    
+    //select your character and then move other characters into the defender section
     
         $(".charContainer").on("click", function () {
             $(".gameText").show();
@@ -23,25 +35,33 @@ $(document).ready( function() {
                 charChosen = yourChar;
                 charSelected = true;
                     if (charChosen === yourChar) {
+                        //have selected character move into attack zone
                         $("#" + yourChar).removeClass("charContainer").addClass("selectedCharContainer").appendTo("#yourChar");
+                        //assigns the value from charObject to charChosen
+                        charChosen = charObject[charChosen];
+                        console.log(charChosen);
                     }
                     if (charSelected === true) {
+                        //move remaining characters into defenders selection area
                         $(".charContainer").removeClass("charContainer").addClass("defenderContainer").appendTo("#defenders");
                     }
             }
-    
+            
             $(".defenderContainer").on("click", function () {
                 if (defSelected === false) {
                     var yourOp = this.id;
                     defChosen = yourOp;
                     defSelected = true;
                     if (defChosen === yourOp) {
+                        //have selected enemy move into the opponent area and changes the opponent's id to opponent specific ones
                         $("#" + yourOp).appendTo("#opponent");
+                        //assigns the value from charObject to defChosen
+                        defChosen = charObject[defChosen];
+                        console.log(defChosen);
                         $("#attack-btn").show();
                     }
                 }
             });
-            
         });
     
     $("#attack-btn").on("click", function () {
@@ -50,6 +70,21 @@ $(document).ready( function() {
         }
         else if ( (charSelected === true) && (defSelected === true) ) {
             console.log("i clicked the attack btn");
+    
+            defChosen[0].hp -= charChosen[0].attack;
+            console.log(defChosen[0].hp + " this is the defenders hp");
+            $("#opponentHealth").html(defChosen[0].hp);
+            
+    
+            console.log(charChosen[0].attack + " this is the starting attack power");
+            charChosen[0].attack += charChosen[0].attack;
+            console.log(charChosen[0].attack + " this is the added attack power");
+    
+          
+           
+            
+            
+            // charChosen.hp -= defChosen.counterAttack;
             
             // charChosen attacks defChosen
             // defChosen counterattacks charChosen
@@ -69,15 +104,6 @@ $(document).ready( function() {
         }
     
     });
-    
-
-    
-    
-    //generate hp attack power and counter attack power for all four characters
-    
-    //select character have selected character move into attack zone
-    
-    //select enemy and have selected enemy move into the defenders zone
     
     //have character player attack (and increase attack power by attack power every time) have enemy counter attack automatically
     
