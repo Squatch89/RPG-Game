badcb52163629b8469eb9965b52f4c69ca13ec38
Gene Williams <GeneWillb@Gmail.com>
2017-08-30 10:34:08 -0400

    reset button working, need to be able to repick your character again

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,25 @@
 $("#attack-btn").hide();
 $(".gameText").hide();
 $("#reset").hide();
+$("#gameOverText").hide()
 
 $(document).ready( function() {
+    var yourChar;
     var charChosen;
     var charSelected = false;
+    var yourOp;
     var defChosen;
     var defSelected = false;
     
     //assign attack hp and counterAttack to each character
     var charObject = {
         char1: [{attack: 18, hp: 150, counterAttack: 9}],
-        char2: [{attack: 8, hp: 100, counterAttack: 5}],
+        char2: [{attack: 1, hp: 100, counterAttack: 5}],
         char3: [{attack: 22,  hp: 200,  counterAttack: 18}],
         char4: [{attack: 12,  hp: 250, counterAttack: 15}]
     };
     
-    //reset function
-    // function reset() {
-    //     charChosen;
-    //     charSelected = false;
-    //     defChosen;
-    //     defSelected = false;
-    //     $("#char1Health").html(charObject.char1[0].hp);
-    //     $("#char2Health").html(charObject.char2[0].hp);
-    //     $("#char3Health").html(charObject.char3[0].hp);
-    //     $("#char4Health").html(charObject.char4[0].hp);
-    // }
+
     
     //displays the characters health
     $("#char1Health").html(charObject.char1[0].hp);
@@ -38,8 +31,9 @@ $(document).ready( function() {
         $(".charContainer").on("click", function () {
             $(".gameText").show();
             $("#selectChar").hide();
+            
             if (charSelected === false) {
-                var yourChar = this.id;
+                yourChar = this.id;
                 charChosen = yourChar;
                 charSelected = true;
                     if (charChosen === yourChar) {
@@ -59,9 +53,10 @@ $(document).ready( function() {
             }
             $(".defenderContainer").on("click", function () {
                 if (defSelected === false) {
-                    var yourOp = this.id;
+                    yourOp = this.id;
                     defChosen = yourOp;
                     defSelected = true;
+                    
                     if (defChosen === yourOp) {
                         
                         //have selected enemy move into the opponent area
@@ -84,31 +79,68 @@ $(document).ready( function() {
             console.log("there's no opponent yet");
         }
         else if ( (charSelected === true) && (defSelected === true) ) {
-    
+
             defChosen[0].hp -= charChosen[0].attack;
             $("#defender-area").find(".health").html(defChosen[0].hp);
-    
-    
+
+
             //charChosen attacks defChosen
             charChosen[0].attack += charChosen[0].attack;
             console.log(charChosen[0].attack + " this is the added attack power");
-    
-    
+
+
             if (defChosen[0].hp >= 0) {
                 //defChosen counterattacks charChosen
                 charChosen[0].hp -= defChosen[0].counterAttack;
                 $(".selectedCharContainer").find(".health").html(charChosen[0].hp);
             }
-            
+            if ( (defChosen[0].hp <= 0) ) {
+                defSelected = false;
+                $("#opponent").empty();
+            }
+            else if (charChosen[0].hp <= 0) {
+                $("#yourChar").hide();
+                $("#attack-btn").hide();
+                $(".gameText").hide();
+                $("#defender-area").hide();
+                $("#defenders-selection").hide();
+                $("#gameOverText").show();
+                $("#reset").show();
+            }
         }
+        
+    });
     
-        if ( (defChosen[0].hp <= 0) ) {
+    $("#reset").on("click", function () {
+            yourChar = "";
+            charChosen = "";
+            charSelected = false;
+            yourOp = "";
+            defChosen = "";
             defSelected = false;
-            $("#defender-area").remove();
-        }
-        else if (charChosen[0].hp <= 0) {
-            $("#reset").show();
-        }
+            charObject = {
+                char1: [{attack: 18, hp: 150, counterAttack: 9}],
+                char2: [{attack: 1, hp: 100, counterAttack: 5}],
+                char3: [{attack: 22,  hp: 200,  counterAttack: 18}],
+                char4: [{attack: 12,  hp: 250, counterAttack: 15}]
+                };
+            $("#char1Health").html(charObject.char1[0].hp);
+            $("#char2Health").html(charObject.char2[0].hp);
+            $("#char3Health").html(charObject.char3[0].hp);
+            $("#char4Health").html(charObject.char4[0].hp);
+            $("#char1").removeClass().addClass("charContainer").appendTo("#selection-area");
+            $("#char2").removeClass().addClass("charContainer").appendTo("#selection-area");
+            $("#char3").removeClass().addClass("charContainer").appendTo("#selection-area");
+            $("#char4").removeClass().addClass("charContainer").appendTo("#selection-area");
+            $("#yourChar").empty();
+            $("#yourChar").show();
+            $("#opponent").empty();
+            $("#selectChar").show();
+            $("#defender-area").show();
+            $("#defenders-selection").show();
+            $("#gameOverText").hide();
+            $("#reset").hide();
+        
     });
     
     
