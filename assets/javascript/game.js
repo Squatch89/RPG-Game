69ff550e2267e0562f8e0f5c6b27ba6638b823d7
Gene Williams <GeneWillb@Gmail.com>
2017-08-29 22:44:29 -0400

    health will now update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,6 +19,7 @@ $(document).ready( function() {
         char4: [{attack: 12,  hp: 250, counterAttack: 15}]
     };
     
+    
     //displays the characters health
     $("#char1Health").html(charObject.char1[0].hp);
     $("#char2Health").html(charObject.char2[0].hp);
@@ -35,13 +36,16 @@ $(document).ready( function() {
                 charChosen = yourChar;
                 charSelected = true;
                     if (charChosen === yourChar) {
+                        
                         //have selected character move into attack zone
                         $("#" + yourChar).removeClass("charContainer").addClass("selectedCharContainer").appendTo("#yourChar");
+                        
                         //assigns the value from charObject to charChosen
                         charChosen = charObject[charChosen];
                         console.log(charChosen);
                     }
                     if (charSelected === true) {
+                        
                         //move remaining characters into defenders selection area
                         $(".charContainer").removeClass("charContainer").addClass("defenderContainer").appendTo("#defenders");
                     }
@@ -53,10 +57,13 @@ $(document).ready( function() {
                     defChosen = yourOp;
                     defSelected = true;
                     if (defChosen === yourOp) {
+                        
                         //have selected enemy move into the opponent area and changes the opponent's id to opponent specific ones
                         $("#" + yourOp).appendTo("#opponent");
+                        
                         //assigns the value from charObject to defChosen
                         defChosen = charObject[defChosen];
+                        
                         console.log(defChosen);
                         $("#attack-btn").show();
                     }
@@ -64,6 +71,8 @@ $(document).ready( function() {
             });
         });
     
+    
+    //need to update health classes for the selected opponent
     $("#attack-btn").on("click", function () {
         if ( (charSelected !== true) || (defSelected !== true) ) {
             console.log("there's no opponent yet");
@@ -73,24 +82,23 @@ $(document).ready( function() {
     
             defChosen[0].hp -= charChosen[0].attack;
             console.log(defChosen[0].hp + " this is the defenders hp");
-            $("#opponentHealth").html(defChosen[0].hp);
-            
+            $(".defenderContainer").find(".health").html(defChosen[0].hp);
     
+    
+            //charChosen attacks defChosen
             console.log(charChosen[0].attack + " this is the starting attack power");
             charChosen[0].attack += charChosen[0].attack;
             console.log(charChosen[0].attack + " this is the added attack power");
     
+    
+            //defChosen counterattacks charChosen
+            charChosen[0].hp -= defChosen[0].counterAttack;
+            console.log(charChosen[0].hp + " this is the new hp of my character");
+            $(".selectedCharContainer").find(".health").html(charChosen[0].hp);
+            
           
-           
             
             
-            // charChosen.hp -= defChosen.counterAttack;
-            
-            // charChosen attacks defChosen
-            // defChosen counterattacks charChosen
-            // attack += attack; for charChosen
-            // defChosen hp - attack
-            // charChosen hp - attack
             //
             // if (defChosen hp = 0){
             //     defSelected = false;
