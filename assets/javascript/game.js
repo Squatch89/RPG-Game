9fdea65b17a67ba70196d270acc683f61a767990
Gene Williams <GeneWillb@Gmail.com>
2017-08-30 23:33:40 -0400

    game resets but is broken on reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,27 @@
 $("#attack-btn").hide();
 $(".gameText").hide();
 $("#reset").hide();
-$("#gameOverText").hide()
+$("#gameOverText").hide();
+$("#youWinText").hide();
+
+var yourChar = undefined;
+var charChosen = undefined;
+var charSelected = false;
+var yourOp = undefined;
+var defChosen = undefined;
+var defSelected = false;
+var wins = 0;
+
+//assign attack hp and counterAttack to each character
+var charObject = {
+    char1: [{attack: 18, hp: 150, counterAttack: 9}],
+    char2: [{attack: 1, hp: 100, counterAttack: 5}],
+    char3: [{attack: 22,  hp: 200,  counterAttack: 18}],
+    char4: [{attack: 12,  hp: 250, counterAttack: 15}]
+};
+
 
 $(document).ready( function() {
-    var yourChar;
-    var charChosen;
-    var charSelected = false;
-    var yourOp;
-    var defChosen;
-    var defSelected = false;
-    var wins = 0;
-    
-    //assign attack hp and counterAttack to each character
-    var charObject = {
-        char1: [{attack: 18, hp: 150, counterAttack: 9}],
-        char2: [{attack: 1, hp: 100, counterAttack: 5}],
-        char3: [{attack: 22,  hp: 200,  counterAttack: 18}],
-        char4: [{attack: 12,  hp: 250, counterAttack: 15}]
-    };
-    
-
     
     //displays the characters health
     $("#char1Health").html(charObject.char1[0].hp);
@@ -28,8 +29,11 @@ $(document).ready( function() {
     $("#char3Health").html(charObject.char3[0].hp);
     $("#char4Health").html(charObject.char4[0].hp);
     
+    
+});
     //select your character and then move other characters into the defender section
         $(".charContainer").on("click", function () {
+            
             $(".gameText").show();
             $("#selectChar").hide();
             
@@ -37,6 +41,7 @@ $(document).ready( function() {
                 yourChar = this.id;
                 charChosen = yourChar;
                 charSelected = true;
+                
                     if (charChosen === yourChar) {
                         
                         //have selected character move into attack zone
@@ -44,8 +49,9 @@ $(document).ready( function() {
                         
                         //assigns the value from charObject to charChosen
                         charChosen = charObject[charChosen];
-                        console.log(charChosen);
+                        console.log(charChosen + " this is my chosen character");
                     }
+                    
                     if (charSelected === true) {
                         
                         //move remaining characters into defenders selection area
@@ -66,7 +72,7 @@ $(document).ready( function() {
                         //assigns the value from charObject to defChosen
                         defChosen = charObject[defChosen];
                         
-                        console.log(defChosen);
+                        console.log(defChosen + " this is my chosen opponent");
                         $("#attack-btn").show();
                     }
                 }
@@ -80,12 +86,12 @@ $(document).ready( function() {
             console.log("there's no opponent yet");
         }
         else if ( (charSelected === true) && (defSelected === true) ) {
-
+    
+            //charChosen attacks defChosen
             defChosen[0].hp -= charChosen[0].attack;
             $("#defender-area").find(".health").html(defChosen[0].hp);
-
-
-            //charChosen attacks defChosen
+            
+            //charChosen has it's attack power increased
             charChosen[0].attack += charChosen[0].attack;
             console.log(charChosen[0].attack + " this is the added attack power");
 
@@ -95,16 +101,15 @@ $(document).ready( function() {
                 charChosen[0].hp -= defChosen[0].counterAttack;
                 $(".selectedCharContainer").find(".health").html(charChosen[0].hp);
             }
+            
             if ( (defChosen[0].hp <= 0) ) {
                 defSelected = false;
                 $("#opponent").empty();
                 wins++;
-                if (wins === 3) {
-                    $("#reset").show();
-                }
             }
             else if (charChosen[0].hp <= 0) {
-                $("#yourChar").hide();
+                $("#selection-area").hide();
+                $("#attack-area").hide();
                 $("#attack-btn").hide();
                 $(".gameText").hide();
                 $("#defender-area").hide();
@@ -112,17 +117,28 @@ $(document).ready( function() {
                 $("#gameOverText").show();
                 $("#reset").show();
             }
+            // if (wins === 3) {
+            //     $("#yourChar").hide();
+            //     $("#attack-btn").hide();
+            //     $(".gameText").hide();
+            //     $("#defender-area").hide();
+            //     $("#defenders-selection").hide();
+            //     $("#youWinText").show();
+            //     $("#reset").show();
+            // }
         }
         
     });
     
     $("#reset").on("click", function () {
-            yourChar = "";
-            charChosen = "";
+            yourChar = undefined;
+            charChosen = undefined;
             charSelected = false;
-            yourOp = "";
-            defChosen = "";
+            yourOp = undefined;
+            defChosen = undefined;
             defSelected = false;
+            this.id = undefined;
+            wins = 0;
             charObject = {
                 char1: [{attack: 18, hp: 150, counterAttack: 9}],
                 char2: [{attack: 1, hp: 100, counterAttack: 5}],
@@ -137,6 +153,8 @@ $(document).ready( function() {
             $("#char2").removeClass().addClass("charContainer").appendTo("#selection-area");
             $("#char3").removeClass().addClass("charContainer").appendTo("#selection-area");
             $("#char4").removeClass().addClass("charContainer").appendTo("#selection-area");
+            $("#selection-area").show();
+            $("#attack-area").show();
             $("#yourChar").empty();
             $("#yourChar").show();
             $("#opponent").empty();
@@ -146,6 +164,14 @@ $(document).ready( function() {
             $("#gameOverText").hide();
             $("#reset").hide();
         
+            console.log("these all happen after I hit reset");
+            console.log(charChosen);
+            console.log(charSelected);
+            console.log(defChosen);
+            console.log(defSelected);
+            console.log(yourChar);
+            console.log(yourOp);
+            
     });
     
     
@@ -157,5 +183,4 @@ $(document).ready( function() {
     
     //if player character is defeated reset game
     
-    
-});
+
